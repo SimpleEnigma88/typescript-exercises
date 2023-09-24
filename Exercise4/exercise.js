a40dec553b48f1b1ac476376e04550befc4fae87
Robert Andrew Moore <ra.moore573@gmail.com>
2023-09-24 18:00:30 -0500

    Doe w/ ex4

---
+++ Exercise4/exercise.js
@@ -0,0 +1,7 @@
+"use strict";
+function greet(name) {
+    return `Hello ${name}! How are you today!`;
+}
+console.log(greet(4)); //Still works. So data validation will still be on the programmer to verify.
+
+console.log(greet('Jinx'));
