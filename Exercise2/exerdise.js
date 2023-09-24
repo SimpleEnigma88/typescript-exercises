7c01aa999874199ce5eb4842376af0d3e73dc81e
Robert Andrew Moore <ra.moore573@gmail.com>
2023-09-24 17:44:38 -0500

    done w/ ex2

---
+++ Exercise2/exerdise.js
@@ -0,0 +1,6 @@
+"use strict";
+let subjects;
+let student;
+student = ["Andy", 4];
+student[0] = "bob";
+console.log(student);
---
+++ Exercise2/exerdise.ts
@@ -0,0 +1,11 @@
+let subjects: string[];
+
+let student: [string, number];
+
+
+
+
+//testing
+student = ["Andy", 4];
+student[0] = "bob";
+console.log(student);
